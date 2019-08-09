6354b327bf5d59cf7d7638bf999adc807a90a739
Alexander Einshøj Lund <alexander.lund1@gmail.com>
2019-08-09 21:06:33 +0200

    finishing the code

--- ruby.rb
+++ ruby.rb
@@ -1,80 +1,63 @@
 module Enumerable
-    def my_each
-        self.length.times {|i| yield(self[i])}
-    end
+  def my_each
+    self.length.times { |i| yield(self[i]) }
+  end
 
-    def my_each_with_index
-        self.length.times {|i| yield(self[i], i)}
-    end
+  def my_each_with_index
+    self.length.times { |i| yield(self[i], i) }
+  end
 
-    def my_select
-        new_array = []
-        self.my_each {|i| new_array << i if yield(i)}
-        return new_array
-    end
+  def my_select
+    new_array = []
+    self.my_each { |i| new_array << i if yield(i) }
+    return new_array
+  end
 
-    def my_all?
-        self.my_each {|i| return false if !yield(i)}
-        return true
-    end
+  def my_all?
+    self.my_each { |i| return false if !yield(i) }
+    return true
+  end
 
-    def my_any?
-        self.my_each {|i| return true if yield(i)}
-        return false
-    end
+  def my_any?
+    self.my_each { |i| return true if yield(i) }
+    return false
+  end
 
-    def my_none?
-        self.my_each {|i| return false if yield(i)}
-        return true
-    end
+  def my_none?
+    self.my_each { |i| return false if yield(i) }
+    return true
+  end
 
-    def my_count(item = nil)
-        count = 0
-        if item != nil
-          self.my_each{ |i| count += 1 if i == item }
-        elsif block_given?
-          self.my_each{ |i| count += 1 if yield(i)}
-        else
-          count = self.length 
-        end
-        return count
+  def my_count(item = nil)
+    count = 0
+    if item != nil
+      self.my_each { |i| count += 1 if i == item }
+    elsif block_given?
+      self.my_each { |i| count += 1 if yield(i) }
+    else
+      count = self.length
     end
+    return count
+  end
 
-    def my_map(proc = nil)
-        new_array = []
-        if proc.nil?
-            self.my_each {|i| new_array << yield(i)}
-        elsif proc != nil && block_given?
-            self.my_each {|i| new_array << proc.call(i)}
-        else
-            self.my_each {|i| new_array << proc.call(i)}
-        end
-        return new_array
+  def my_map(proc = nil)
+    new_array = []
+    if proc.nil?
+      self.my_each { |i| new_array << yield(i) }
+    elsif proc != nil && block_given?
+      self.my_each { |i| new_array << proc.call(i) }
+    else
+      self.my_each { |i| new_array << proc.call(i) }
     end
+    return new_array
+  end
 
-    def my_inject(result = 0)
-        self.my_each {|i|  result = yield(result, i)}
-        return result  
-    end
+  def my_inject(result = 0)
+    self.my_each { |i| result = yield(result, i) }
+    return result
+  end
 end
 
 def multiply_els(array)
-    array.my_inject(1) {|sum, i| sum * i}
+  array.my_inject(1) { |sum, i| sum * i }
 end
-
-
-
-
-p = Proc.new { |x| x + 1 }
-
-
-
-arr = [1,2,3,4]
-
-l = arr.my_map(&p) #{|s| s + 1}
-
-#l = arr.my_inject {|s,i| s + i}
-
-#l = multiply_els(arr)
-
-print l
