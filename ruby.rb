233bef56ce7b2b1abd14a9edf750e6697c168e56
Alexander Einshøj Lund <alexander.lund1@gmail.com>
2019-08-11 21:57:35 +0200

    final changes made

--- ruby.rb
+++ ruby.rb
@@ -3,42 +3,42 @@
 # Enumerable module
 module Enumerable
   def my_each
-    self.length.times { |i| yield(self[i]) }
+    length.times { |i| yield(self[i]) }
   end
 
   def my_each_with_index
-    self.length.times { |i| yield(self[i], i) }
+    length.times { |i| yield(self[i], i) }
   end
 
   def my_select
     new_array = []
-    self.my_each { |i| new_array << i if yield(i) }
+    my_each { |i| new_array << i if yield(i) }
     new_array
   end
 
   def my_all?
-    self.my_each { |i| return false unless yield(i) }
+    my_each { |i| return false unless yield(i) }
     true
   end
 
   def my_any?
-    self.my_each { |i| return true if yield(i) }
+    my_each { |i| return true if yield(i) }
     false
   end
 
   def my_none?
-    self.my_each { |i| return false if yield(i) }
+    my_each { |i| return false if yield(i) }
     true
   end
 
   def my_count(item = nil)
     count = 0
     if !item.nil?
-      self.my_each { |i| count += 1 if i == item }
+      my_each { |i| count += 1 if i == item }
     elsif block_given?
-      self.my_each { |i| count += 1 if yield(i) }
+      my_each { |i| count += 1 if yield(i) }
     else
-      count = self.length
+      count = length
     end
     count
   end
@@ -46,17 +46,17 @@ module Enumerable
   def my_map(proc = nil)
     new_array = []
     if proc.nil?
-      self.my_each { |i| new_array << yield(i) }
+      my_each { |i| new_array << yield(i) }
     elsif !proc.nil? && block_given?
-      self.my_each { |i| new_array << proc.call(i) }
+      my_each { |i| new_array << proc.call(i) }
     else
-      self.my_each { |i| new_array << proc.call(i) }
+      my_each { |i| new_array << proc.call(i) }
     end
     new_array
   end
 
   def my_inject(result = 0)
-    self.my_each { |i| result = yield(result, i) }
+    my_each { |i| result = yield(result, i) }
     result
   end
 end
